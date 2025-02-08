582601e15ab020810e528a7282d9cfff63be3195
Nathan Will Martins <nathanwillmartins@gmail.com>
2025-02-08 15:08:30 -0300

    feat: Inicio da main page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="#" method="POST">
+        @csrf
+        <label>Email:</label>
+        <input type="email" name="email" required>
+
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrganizadorController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//Pagina Inicial
+Route::get('/', function () { return view('welcome'); });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Login
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+//Organizador
+Route::get('/cadastroOrganizador', [OrganizadorController::class, 'index']);
+Route::post('/cadastroOrganizador', [OrganizadorController::class, 'store']);
